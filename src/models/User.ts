8434acb3ae033ddc8219bc2a8c3e99115e55b2af
정규현 (Jacob) <kh.jung@ecubelabs.com>
2020-01-11 17:50:01 +0900

    Add user-schema

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,35 +1,42 @@
-import { ObjectType, Field, ID } from 'type-graphql';
+import { ObjectType, Field, ID, Int } from 'type-graphql';
+import { Min, Max, MaxLength } from 'class-validator';
 
-/**
- * User 스키마
- */
-
-@ObjectType()
+@ObjectType({ description: 'user 스키마 모델' })
 export class User {
   @Field(type => ID)
   readonly id: string;
 
-  @Field()
+  @Field({ description: '이메일' })
+  @MaxLength(255)
   email: string;
 
-  @Field()
+  @Field({ description: '암호' })
+  @MaxLength(255)
   password: string;
 
-  @Field()
+  @Field({ description: '닉네임 ex) 동쪽에서 번쩍이는 홍길동' })
+  @MaxLength(255)
   nickName: string;
 
-  @Field()
+  @Field({ description: '유저 이름 ex) 홍길동' })
+  @MaxLength(20)
   userName: string;
 
-  @Field({ nullable: true })
+  @Field(type => Int, {
+    nullable: true,
+    description: '(비트값) 1 - 사용가능, 0 - 사용 불가능',
+  })
+  @Min(0)
+  @Max(1)
   enabled?: number;
 
-  @Field({ nullable: true })
+  @Field({ nullable: true, description: '유저 img URL' })
+  @MaxLength(255)
   userImage?: string;
 
-  @Field({ nullable: true })
+  @Field({ nullable: true, description: '(자동) 계정 생성일' })
   readonly dateCreated?: Date;
 
-  @Field({ nullable: true })
+  @Field({ nullable: true, description: '(자동) 계정 마지막 수정일' })
   readonly lastUpdated?: Date;
 }
